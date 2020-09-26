6f5623eb18a5a4959f5ffce35a684415ffd21b9f
AV3110 <akshansh3110@gmail.com>
2020-09-26 07:16:19 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,10 @@
-# Module 1 - Create a Blockchain
 
-# To be installed:
-# Flask==0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com/
-
-# Importing the libraries
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-# Part 1 - Building a Blockchain
+
 
 class Blockchain:
 
@@ -60,15 +54,15 @@ class Blockchain:
             block_index += 1
         return True
 
-# Part 2 - Mining our Blockchain
 
-# Creating a Web App
+
+
 app = Flask(__name__)
 
-# Creating a Blockchain
+
 blockchain = Blockchain()
 
-# Mining a new block
+
 @app.route('/mine_block', methods = ['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
@@ -83,14 +77,13 @@ def mine_block():
                 'previous_hash': block['previous_hash']}
     return jsonify(response), 200
 
-# Getting the full Blockchain
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
-# Checking if the Blockchain is valid
+
 @app.route('/is_valid', methods = ['GET'])
 def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
@@ -100,5 +93,5 @@ def is_valid():
         response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
-# Running the app
+
 app.run(host = '0.0.0.0', port = 5000)
